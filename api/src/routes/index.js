29d8f8f8b78f46db2bce8dafcd02078336c7909e
NazaMaestre <Nazarenomaestre6@gmail.com>
2022-12-10 16:29:47 -0300

    NavBar

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,5 @@
 const { Router } = require("express");
 
-const categoriesRouter = require("./categoriesRoute");
-const productsRouter = require("./productsRoute");
 const router = Router();
-
-router.use("/categories", categoriesRouter);
-router.use("/products", productsRouter);
 
 module.exports = router;
