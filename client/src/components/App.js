bd585caea4a71b3f6e91ae96e56d0ae5b6aafbdb
Danilo <daniloud2809@gmail.com>
2022-12-10 16:55:54 -0500

    Navbar 2.0

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Home from "./Home/Home";
-import NavBar from "./NavBar/NavBar";
+import NavBar from "./NavBar/NavBar.jsx";
 
 function App() {
   return (
     <div>
-      <NavBar />;
-      <Home />;
+      <NavBar />
+      <Home />
     </div>
   );
 }
