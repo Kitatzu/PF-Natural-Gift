dcfec629e0cc4b3db7ed364d18006ed7f5fd42d2
Kitatzu <exe922@gmail.com>
2022-12-11 18:57:49 -0300

    Delete Route Backend

--- api/src/models/Categories.js
+++ api/src/models/Categories.js
@@ -3,8 +3,8 @@ const { DataTypes, UUIDV4 } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("categories", {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: UUIDV4,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
       primaryKey: true,
     },
     name: {
--- api/src/models/Products.js
+++ api/src/models/Products.js
@@ -1,35 +1,42 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("products", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    "products",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imageProduct: {
+        type: DataTypes.STRING,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      inDatabase: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    imageProduct: {
-      type: DataTypes.STRING,
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-    },
-    rating: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-    },
-    inDatabase: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-  });
+    {
+      paranoid: true,
+      timestamp: true,
+    }
+  );
 };
