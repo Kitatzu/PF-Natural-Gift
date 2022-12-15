6bc8d44dcf89c48617bdb6bf864f5d0baa1a690f
NazaMaestre <Nazarenomaestre6@gmail.com>
2022-12-15 19:21:30 -0300

    FormRegister

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,9 +1,11 @@
 import FormLogin from "./FormLogin/FormLogin";
+import FormRegister from "./FormRegister/FormRegister";
 import Nav from "./Nav/Nav";
 import "./Login.scss";
 const Login = () => {
   return (
     <div className="Login-container">
+      <FormRegister />
       <Nav />
     </div>
   );
