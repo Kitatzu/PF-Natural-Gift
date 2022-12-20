e92e35e91b91367531471e56d95d7fdb34a48ce4
Danilo <daniloud2809@gmail.com>
2022-12-20 17:09:08 -0500

    Banner responsive done

---
+++ client/src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Banner.scss";
+
+const Banner = () => {
+  return (
+    <div className="Banner">
+      <div className="Banner-img1"></div>
+      <div className="Banner-img2"></div>
+      <div className="Banner-img2"></div>
+      <div className="Banner-img2"></div>
+      <div className="Banner-img2"></div>
+      <div className="Banner-img2"></div>
+      <div className="Banner-img2"></div>
+  </div>
+  )
+}
+
+export default Banner;
