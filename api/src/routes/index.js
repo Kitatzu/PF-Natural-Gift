18632b33a84ba77e4e38b7efc9434a9da062a068
adrian4058 <adrian_2016_@outlook.es>
2022-12-22 18:45:53 -0300

    finished cart and transactions in process

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,8 @@ const categories = require("../routes/categoriesRoutes");
 const register = require("../routes/registerRoutes");
 const login = require("../routes/loginRoutes");
 const users = require("../routes/usersRoutes");
+const cart = require("../routes/cart");
+const transaction = require("../routes/Transaction");
 const router = Router();
 
 router.use("/products", products);
@@ -11,5 +13,7 @@ router.use("/categories", categories);
 router.use("/register", register);
 router.use("/login", login);
 router.use("/users", users);
+router.use("/cart", cart);
+router.use("/transaction", transaction);
 
 module.exports = router;
