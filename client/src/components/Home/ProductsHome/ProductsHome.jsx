b46fdddc21e99e751346a67d9008b52f09933a7c
Danilo <daniloud2809@gmail.com>
2022-12-23 22:26:57 -0500

    GetProducts done

--- client/src/components/Home/ProductsHome/ProductsHome.jsx
+++ client/src/components/Home/ProductsHome/ProductsHome.jsx
@@ -1,19 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../../Redux/Thunks";
 import ProductsCards from "../../ProductsCards/ProductsCards";
 import "./ProductsHome.scss";
 
 const ProductsHome = () => {
+
+  const { isLoading, products = []} = useSelector( state => state.products );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [])
+
   return (
     <div className="ProductsHome">
       <div className="ProductsHome-title">
         <h1>Dale un vistazo a nuestros productos</h1>
       </div>
       <div className="Products-cards">
-        <ProductsCards/>
-        <ProductsCards/>
-        <ProductsCards/>
-        <ProductsCards/>
-        <ProductsCards/>
-        <ProductsCards/>
+        {
+          products.map(product => (
+            <ProductsCards
+              name = {product.name}
+              price = {product.price}
+              image = {product.imageProduct}
+            />
+          ))
+        }
       </div> 
     </div>
   )
