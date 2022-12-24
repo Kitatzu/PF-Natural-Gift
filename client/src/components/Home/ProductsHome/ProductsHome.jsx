92540a12f3941609e2bcc68d13eb3bb5fb38f6c1
Jhonatan Iñiguez <73409535+copaTech-98@users.noreply.github.com>
2022-12-24 01:15:23 -0500

    fix(registerLogin)

--- client/src/components/Home/ProductsHome/ProductsHome.jsx
+++ client/src/components/Home/ProductsHome/ProductsHome.jsx
@@ -1,17 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../../../Redux/Thunks";
+import { getProducts } from "../../../Redux/Thunks/index";
 import ProductsCards from "../../ProductsCards/ProductsCards";
 import "./ProductsHome.scss";
 
 const ProductsHome = () => {
-
-  const { isLoading, products = []} = useSelector( state => state.products );
+  const { isLoading, products = [] } = useSelector((state) => state.products);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getProducts());
-  }, [])
+  }, []);
 
   return (
     <div className="ProductsHome">
@@ -19,18 +18,16 @@ const ProductsHome = () => {
         <h1>Dale un vistazo a nuestros productos</h1>
       </div>
       <div className="Products-cards">
-        {
-          products.map(product => (
-            <ProductsCards
-              name = {product.name}
-              price = {product.price}
-              image = {product.imageProduct}
-            />
-          ))
-        }
-      </div> 
+        {products.map((product) => (
+          <ProductsCards
+            name={product.name}
+            price={product.price}
+            image={product.imageProduct}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductsHome;
