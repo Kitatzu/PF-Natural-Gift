f9734ac03251f8cad3222285a06e6abdf42bd745
Jhonatan Iñiguez <73409535+copaTech-98@users.noreply.github.com>
2022-12-25 14:45:17 -0500

    fix(login)
    fix(
     at login user redirect to home with validate token
    )

--- client/src/Redux/Thunks/LoginUser.js
+++ client/src/Redux/Thunks/LoginUser.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 import Swal from "sweetalert2";
-
+import { setIsLog, setUserName } from "../Slices";
 export const loginUser = (form) => {
-  return async () =>
+  return async (dispatch) =>
     await axios
       .post("http://localhost:3001/login", form)
       .then((data) => {
-        console.log(data);
+        const userData = {
+          userName: form.email,
+          token: data.data.newToken,
+        };
+        localStorage.setItem("token", JSON.stringify(userData));
 
         Swal.fire({
           icon: "success",
           title: "Login Ok!",
           text: "Usuario Logeado correctamente!",
+        }).then(async (response) => {
+          await dispatch(setUserName(form.email));
+          await dispatch(setIsLog(data.data.newToken));
         });
       })
       .catch((response) => {
