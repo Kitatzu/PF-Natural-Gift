2e4b22b5c41950bf20e35dfbdb6a421cb6bc5339
Kitatzu <exe922@gmail.com>
2022-12-26 23:59:38 -0300

    Fix Cloudinary Avatar and ImageProduct

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 require("./db.js");
 
@@ -14,6 +15,7 @@ server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
+server.use(cors());
 server.use(
   fileUpload({
     useTempFiles: true,
--- api/src/models/Products.js
+++ api/src/models/Products.js
@@ -16,7 +16,6 @@ module.exports = (sequelize) => {
       },
       imageProduct: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       description: {
         type: DataTypes.STRING,
