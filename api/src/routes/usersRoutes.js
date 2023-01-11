35a2401a80f1a03c4844ffc197f8013c880c56f6
Kitatzu <exe922@gmail.com>
2023-01-11 16:26:41 -0300

    Routes restore and view deleted users and product

--- api/src/routes/usersRoutes.js
+++ api/src/routes/usersRoutes.js
@@ -1,14 +1,18 @@
 const { Router } = require("express");
 const {
   allUsers,
+  allUSersDeleted,
   deleteUser,
+  restoreUser,
   updateUser,
 } = require("../controller/usersController.js");
 const fileUpload = require("express-fileupload");
 const router = Router();
 
 router.get("/", allUsers);
+router.get("/deleted", allUSersDeleted);
 router.delete("/:id", deleteUser);
+router.get("/restore/:id", restoreUser);
 router.put(
   "/:id",
   fileUpload({
