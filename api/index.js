b7f137705ee7779ac2a2428857d28c7283c0bc09
Exequiel Rodriguez <109878818+Kitatzu@users.noreply.github.com>
2023-01-11 16:57:17 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ server.listen(PORT, () => {
   console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   try {
     conn
-      .sync({force: true})
+      .sync()
       .then((response) => {
         createRoles();
       })
