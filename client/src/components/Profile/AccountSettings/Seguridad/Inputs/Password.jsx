75755e76aafe720194b5024640626192ab6b9787
Danilo <daniloud2809@gmail.com>
2023-01-11 01:11:40 -0500

    change password, falta thunk

--- client/src/components/Profile/AccountSettings/Seguridad/Inputs/Password.jsx
+++ client/src/components/Profile/AccountSettings/Seguridad/Inputs/Password.jsx
@@ -7,8 +7,10 @@ import {
 } from "@mui/material";
 import { Icon } from "@iconify/react";
 import React from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
-const Password = ({ name, label }) => {
+
+const Password = ({ name, label, inputRef }) => {
   const mode = useSelector((store) => store.theme.mode);
   const Theme = useSelector((store) => store.theme);
   const [showPassword, setShowPassword] = React.useState(false);
@@ -18,6 +20,15 @@ const Password = ({ name, label }) => {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+
+  const [pass, setPass] = useState('');
+
+  const handlePassChange = (e) => {
+    setPass(e.target.value)
+  }
+
+  console.log(pass);
+  
   return (
     <FormControl
       sx={{ m: 1, width: "100%", margin: "10px 0" }}
@@ -57,6 +68,9 @@ const Password = ({ name, label }) => {
           </InputAdornment>
         }
         label={label}
+        onChange={handlePassChange}
+        value={pass}
+        ref={inputRef}
       />
     </FormControl>
   );
